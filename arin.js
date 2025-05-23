be1a499da33aa67ca8308f5d9605f57a36266dbb
Fatir Ahmad <djfdat@gmail.com>
2025-05-23 03:12:49 -0400

    Fixed not finding the nav bar

--- arin.js
+++ arin.js
@@ -19,20 +19,37 @@ const links = [
  */
 function addAnchorToNav() {
 	// Select Amazon's navigation container element
-	const navElement = document.getElementById('nav-xshop');
+	const navElement = document.querySelector("#nav-xshop > ul.nav-ul")
 
+	// Check if the navigation element is found
 	// Error and exit if navigation element isn't found on the page
 	if (!navElement) {
-		console.error('ARIN Nav element not found');
+		console.error('Amazon Nav element not found');
 		return
 	}
 
+	let taxIndex = links.length; // Start tabIndex from the number of links to be added
+
 	// Create and insert new links in reverse order since they are inserted at the beginning
 	[...links].reverse().forEach((element) => {
-		// Create new anchor element and assign properties from links array
-		const newLink = Object.assign(document.createElement('a'), element);
+		// Create a new element li.nav-li > div.nav-div > a.nav-a
+		const newLi = document.createElement('li');
+		newLi.className = 'nav-li';
+
+		const newDiv = document.createElement('div');
+		newDiv.className = 'nav-div';
+
+		const newLink = document.createElement('a');
+		newLink.className = element.className;
+		newLink.href = element.href;
+		newLink.textContent = element.textContent;
+		newLink.tabIndex = 0;
+
+		newDiv.appendChild(newLink);
+		newLi.appendChild(newDiv);
+
 		// Insert the new link at the beginning of the navigation
-		navElement.insertBefore(newLink, navElement.firstChild);
+		navElement.insertBefore(newLi, navElement.firstChild);
 	});
 }
 
